5e85e1a7872937dbec0772ae35b53d66944e013b
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-20 05:46:00 +0900

    add change lang

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,31 @@
 import * as React from "react";
+import { GetServerSideProps } from "next";
 import { ArticleWrapper } from "@sadness.ojisan/reghcss";
+import langParser from "accept-language-parser";
 import { ja } from "../assets/locale/ja";
 import { en } from "../assets/locale/en";
 
 type LangType = "JA" | "EN";
 
-export default () => {
+const LANG_SET: LangType[] = ["JA", "EN"];
+
+export default (props) => {
   const [lang, setLang] = React.useState<LangType>("JA");
+  React.useEffect(() => {
+    const { langSetting }: { langSetting: string } = props;
+    if (isLang(langSetting)) {
+      setLang(langSetting);
+    } else {
+      setLang("JA");
+    }
+  }, []);
   const handleCangeLang = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const lang = e.target.value;
-    // TODO: あとで直す
-    setLang(lang as LangType);
+    if (isLang(lang)) {
+      setLang(lang);
+    } else {
+      setLang("JA");
+    }
   };
   return (
     <div>
@@ -23,4 +38,26 @@ export default () => {
       </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = (context) => {
+  const lang = context.req.headers["accept-language"];
+  const parsedLang = langParser.parse(lang);
+  let langSetting = _langSet(parsedLang[0].code);
+  console.log(langSetting);
+  return { props: { langSetting } };
+};
+
+const _langSet = (value: string): LangType => {
+  switch (value) {
+    case "ja":
+      return "JA";
+    default:
+      return "EN";
+  }
+};
+
+const isLang = (value: string): value is LangType => {
+  // 検査対象なのでanyを使ってもよし
+  return LANG_SET.includes(value as any);
 };
